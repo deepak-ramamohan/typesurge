8712c96d2907c342640daada08d80b228089a7d4
Deepak Ramamohan <deepak.r.drm@gmail.com>
2025-10-09 19:21:36 +0530

    Added a new class for loading and managing words

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import arcade
 import random
 import math
 from pyglet.graphics import Batch
+from collections import defaultdict
 
 
 class GameView(arcade.Window):
     
     def __init__(self, width = 1280, height = 720, title = "Typing Game"):
         super().__init__(width, height, title)
-        # self.word_list = ["hello", "world", "the", "quick", "brown", "fox", "jumps", 
-        #                   "over", "lazy", "dog"]
-        self.word_list = load_words()
+        self.word_manager = WordManager()
         self.player_sprite = None
         self.input = ""
         self.explosion_list = None
@@ -49,7 +48,7 @@ class GameView(arcade.Window):
         count_target = self.ENEMY_COUNT_MIN if random.random() <= 0.8 else self.ENEMY_COUNT_MAX
         while current_enemy_count < count_target:
             enemy_word = EnemyWord(
-                self._get_word(), 
+                self.word_manager.generate_word(min_character_count=4, max_character_count=7), 
                 position=self._get_enemy_spawn_position_at_random(), 
                 target=self.player_sprite.position
             )
@@ -180,9 +179,6 @@ class GameView(arcade.Window):
         key_pressed = key_mapping.get(symbol, "")
         self.input = self.input + key_pressed
 
-    def _get_word(self):
-        return random.choice(self.word_list)
-    
 
 class EnemyWord(arcade.Sprite):
     """
@@ -293,16 +289,33 @@ def calculate_angle(point1, point2):
     return theta
 
 
-def load_words():
-    """
-    Loading the words from the text file.
-    The file was downloaded from:
-    https://github.com/dwyl/english-words/blob/master/words_alpha.txt
-    """
-    word_list = []
-    with open("words_alpha.txt", 'r') as file:
-        word_list = list(set(file.read().split()))
-    return word_list
+class WordManager:
+    """Class for loading and managing words"""
+
+    def __init__(self, file_path="words_v1.txt"):
+        self._load_words(file_path)
+        self._split_word_list_by_character_count()
+
+    def _load_words(self, file_path):
+        """
+        Load the words from the text file.
+        """
+        self.word_list = []
+        with open(file_path, 'r') as file:
+            self.word_list = list(set(file.read().split()))
+    
+    def _split_word_list_by_character_count(self):
+        self.character_count_dict = defaultdict(list)
+        for word in self.word_list:
+            self.character_count_dict[len(word)].append(word)
+
+    def generate_word(self, min_character_count=4, max_character_count=7):
+        """
+        Generate new words by sampling from the existing list
+        """
+        character_count = random.randint(min_character_count, max_character_count)
+        word = random.choice(self.character_count_dict[character_count])
+        return word
 
 
 def main():
