ac7fc8eaf6fe5392f1757cb09374b0eb3a763f3e
Deepak Ramamohan <deepak.r.drm@gmail.com>
2025-10-17 11:41:34 +0530

    Moved the key mapping dictionary to utils/helpers.py

--- space_shooter/views.py
+++ space_shooter/views.py
@@ -4,7 +4,7 @@ import math
 from space_shooter.player import Player
 from space_shooter.enemies import EnemySpawner, EnemyWordList
 from space_shooter.explosion import Explosion
-from utils.helpers import calculate_angle_between_points
+from utils.helpers import calculate_angle_between_points, key_mapping
 from pyglet.graphics import Batch
 from arcade.gui import (
     UIManager,
@@ -157,41 +157,6 @@ class SpaceShooterGameView(arcade.View):
         if symbol == arcade.key.ESCAPE:
             pause_view = PauseView(self)
             self.window.show_view(pause_view)
-        key_mapping = {
-            arcade.key.A: "a",
-            arcade.key.B: "b",
-            arcade.key.C: "c",
-            arcade.key.D: "d",
-            arcade.key.E: "e",
-            arcade.key.F: "f",
-            arcade.key.G: "g",
-            arcade.key.H: "h",
-            arcade.key.I: "i",
-            arcade.key.J: "j",
-            arcade.key.K: "k",
-            arcade.key.L: "l",
-            arcade.key.M: "m",
-            arcade.key.N: "n",
-            arcade.key.O: "o",
-            arcade.key.P: "p",
-            arcade.key.Q: "q",
-            arcade.key.R: "r",
-            arcade.key.S: "s",
-            arcade.key.T: "t",
-            arcade.key.U: "u",
-            arcade.key.V: "v",
-            arcade.key.W: "w",
-            arcade.key.X: "x",
-            arcade.key.Y: "y",
-            arcade.key.Z: "z",
-            arcade.key.COMMA: ",",
-            arcade.key.PERIOD: ".",
-            arcade.key.SLASH: "/",
-            arcade.key.COLON: ";",
-            arcade.key.QUOTELEFT: "'",
-            arcade.key.BRACKETLEFT: "[",
-            arcade.key.BRACKETRIGHT: "]"
-        }
         key_pressed = key_mapping.get(symbol, "")
         self.input = self.input + key_pressed
 
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 import math
+import arcade
 
 
 def calculate_angle_between_points(point1, point2):
@@ -9,3 +10,41 @@ def calculate_angle_between_points(point1, point2):
     dx = point2[0] - point1[0]
     theta = math.atan2(dy, dx)
     return theta
+
+
+key_mapping = {
+    arcade.key.A: "a",
+    arcade.key.B: "b",
+    arcade.key.C: "c",
+    arcade.key.D: "d",
+    arcade.key.E: "e",
+    arcade.key.F: "f",
+    arcade.key.G: "g",
+    arcade.key.H: "h",
+    arcade.key.I: "i",
+    arcade.key.J: "j",
+    arcade.key.K: "k",
+    arcade.key.L: "l",
+    arcade.key.M: "m",
+    arcade.key.N: "n",
+    arcade.key.O: "o",
+    arcade.key.P: "p",
+    arcade.key.Q: "q",
+    arcade.key.R: "r",
+    arcade.key.S: "s",
+    arcade.key.T: "t",
+    arcade.key.U: "u",
+    arcade.key.V: "v",
+    arcade.key.W: "w",
+    arcade.key.X: "x",
+    arcade.key.Y: "y",
+    arcade.key.Z: "z",
+    arcade.key.SPACE: " ",
+    arcade.key.COMMA: ",",
+    arcade.key.PERIOD: ".",
+    arcade.key.SLASH: "/",
+    arcade.key.COLON: ";",
+    arcade.key.QUOTELEFT: "'",
+    arcade.key.BRACKETLEFT: "[",
+    arcade.key.BRACKETRIGHT: "]"
+}
