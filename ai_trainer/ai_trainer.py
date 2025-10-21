302c9be57b036d65889d77e577cacef6c5d997ff
Deepak Ramamohan <deepak.r.drm@gmail.com>
2025-10-21 12:23:42 +0530

    Added pause view to the AI trainer, displaying WPM and number of words typed

--- ai_trainer/ai_trainer.py
+++ ai_trainer/ai_trainer.py
@@ -2,6 +2,14 @@ import arcade
 from space_shooter.word_manager import WordManager
 from utils.textures import SEPIA_BACKGROUND
 from utils.colors import BROWN
+from pyglet.graphics import Batch
+from arcade.gui import (
+    UIManager,
+    UIAnchorLayout,
+    UIFlatButton,
+    UIBoxLayout
+)
+from utils.button_styles import sepia_button_style
 
 
 class AITrainerView(arcade.View):
@@ -9,6 +17,7 @@ class AITrainerView(arcade.View):
     TARGET_TEXT_COLOR = BROWN
     INPUT_TEXT_COLOR = arcade.color.ANTIQUE_BRASS
     WPM_TEXT_COLOR = arcade.color.ANTIQUE_RUBY
+    FONT_NAME = "Pixelzone"
 
     def __init__(self, main_menu_view):
         super().__init__()
@@ -21,7 +30,7 @@ class AITrainerView(arcade.View):
             x=self.window.width//2, 
             y=self.window.height//2 + 50, 
             anchor_x="center",
-            font_name="Pixelzone",
+            font_name=self.FONT_NAME,
             font_size=64,
             color=self.TARGET_TEXT_COLOR,
             bold=True
@@ -31,7 +40,7 @@ class AITrainerView(arcade.View):
             x=self.target_text.x, 
             y=self.target_text.y - 100, 
             anchor_x="center",
-            font_name="Pixelzone",
+            font_name=self.FONT_NAME,
             font_size=64,
             color=self.INPUT_TEXT_COLOR
         )
@@ -46,7 +55,9 @@ class AITrainerView(arcade.View):
         )
         self.wpm = 0
         self.time_elapsed = 0
+        self.words_typed = 0
         self.character_count = 0
+        self.pause_view = PauseView(self)
 
     def setup(self):
         self.target_text.text = self.word_manager.generate_word(min_character_count=4, max_character_count=8)
@@ -68,7 +79,11 @@ class AITrainerView(arcade.View):
         if self._is_input_matching():
             self.character_count += len(self.text_input_buffer)
             self.text_input_buffer = []
-            self.target_text.text = self.word_manager.generate_word(min_character_count=4, max_character_count=8)
+            self.target_text.text = self.word_manager.generate_word(
+                min_character_count=4, 
+                max_character_count=8
+            )
+            self.words_typed += 1
         self._update_wpm(delta_time)
 
     def _reset_wpm(self):
@@ -85,7 +100,8 @@ class AITrainerView(arcade.View):
         
     def on_key_press(self, symbol, modifiers):
         if symbol == arcade.key.ESCAPE:
-            self.window.show_view(self.main_menu_view)
+            pause_view = PauseView(self)
+            self.window.show_view(pause_view)
 
     def on_text(self, text):
         """
@@ -105,3 +121,90 @@ class AITrainerView(arcade.View):
 
     def _is_input_matching(self):
         return self.input_text.text == self.target_text.text
+    
+    def on_show_view(self):
+        self.window.set_mouse_visible(False)
+
+    def on_hide_view(self):
+        self.window.set_mouse_visible(True)
+
+
+class PauseView(arcade.View):
+
+    def __init__(self, game_view):
+        super().__init__()
+        self.game_view = game_view
+        self.text_batch = Batch()
+        self.title_text = arcade.Text(
+            "Game Paused",
+            x = self.window.width // 2,
+            y = self.window.height // 2 + 100,
+            anchor_x="center",
+            font_name=self.game_view.FONT_NAME,
+            font_size=64,
+            color=BROWN,
+            bold=True,
+            batch=self.text_batch
+        )
+        self.score_text = arcade.Text(
+            f"WPM: {self.game_view.wpm}, Words typed: {self.game_view.words_typed}", 
+            x = self.title_text.x,
+            y = self.title_text.y - 70,
+            anchor_x="center",
+            font_name=self.game_view.FONT_NAME,
+            font_size=48,
+            color=BROWN,
+            batch=self.text_batch
+        )
+
+        self.ui = UIManager()
+        self.anchor = self.ui.add(UIAnchorLayout())
+        self.BUTTON_WIDTH = 300
+
+        self.resume_button = UIFlatButton(
+            text="Resume",
+            width=self.BUTTON_WIDTH,
+            style=sepia_button_style
+        )
+        @self.resume_button.event("on_click")
+        def _(event):
+            self._resume()
+
+        self.quit_to_main_menu_button = UIFlatButton(
+            text="Quit to Main Menu",
+            width=self.BUTTON_WIDTH,
+            style=sepia_button_style
+        )
+        @self.quit_to_main_menu_button.event("on_click")
+        def _(event):
+            self._return_to_main_menu()
+
+        self.box_layout = UIBoxLayout(space_between=15)
+        self.box_layout.add(self.resume_button)
+        self.box_layout.add(self.quit_to_main_menu_button)
+        self.anchor.add(
+            self.box_layout,
+            anchor_y="top",
+            align_y=-(self.height - self.score_text.y + 30)
+        )
+
+    def on_show_view(self):
+        self.ui.enable()
+
+    def on_hide_view(self):
+        self.ui.disable()
+
+    def on_draw(self):
+        self.clear() # This is IMPORTANT! The text looks jagged without this!
+        arcade.draw_texture_rect(
+            SEPIA_BACKGROUND,
+            arcade.LBWH(0, 0, self.window.width, self.window.height)
+        )
+        self.text_batch.draw()
+        self.ui.draw()
+
+    def _resume(self):
+        self.window.show_view(self.game_view)
+
+    def _return_to_main_menu(self):
+        self.window.show_view(self.game_view.main_menu_view)
