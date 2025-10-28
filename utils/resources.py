b64f00dd731ef9fa5bb635e9c92111ca2d8d92fa
Deepak Ramamohan <deepak.r.drm@gmail.com>
2025-10-28 21:00:06 +0530

    Removed load_image() function; added sounds

--- utils/resources.py
+++ utils/resources.py
@@ -1,28 +1,10 @@
 import arcade
-from PIL import Image, ImageOps
 from utils.colors import BROWN
-from utils.helpers import tint_image
+from utils.helpers import load_image
 
 
+# Textures
 SEPIA_BACKGROUND = arcade.load_texture("assets/sepia_background.png")
-
-
-def load_image(path: str, invert: bool = True, tint_color: tuple[int, int, int] = None) -> Image.Image:
-    image = Image.open(path)
-    if invert:
-        if image.mode == 'RGBA':
-            r,g,b,a = image.split()
-            rgb_image = Image.merge('RGB', (r,g,b))
-            output_image = ImageOps.invert(rgb_image)
-            r2,g2,b2 = output_image.split()
-            output_image = Image.merge('RGBA', (r2,g2,b2,a))
-        else:
-            output_image = ImageOps.invert(image)
-    if tint_color is not None:
-        output_image = tint_image(output_image, tint_color)
-    return output_image
-
-
 METEOR_SPRITE_1 = arcade.Texture(load_image("assets/sprites/asteroid.png"))
 METEOR_SPRITE_2 = arcade.Texture(load_image("assets/sprites/meteor.png"))
 SPACESHIP_SPRITE = arcade.Texture(load_image("assets/sprites/spaceship_2.png"))
@@ -30,3 +12,8 @@ BULLET_SPRITE = arcade.Texture(load_image("assets/sprites/bullet.png"))
 USER_PROFILE_SPRITE = arcade.Texture(
     load_image("assets/sprites/account.png", tint_color=BROWN[:3])
 )
+
+# Sounds
+MAIN_MENU_MUSIC = arcade.Sound("assets/sounds/hard_boiled.mp3", streaming=True)
+AI_TRAINER_MUSIC = arcade.Sound("assets/sounds/vibing_over_venus.mp3", streaming=True)
+SPACE_SHOOTER_MUSIC = arcade.Sound("assets/sounds/space_jazz.mp3", streaming=True)
