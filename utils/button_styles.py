c43335bb4d21f64b7bd38b869abd5a0fd512c1a5
Deepak Ramamohan <deepak.r.drm@gmail.com>
2025-10-28 09:10:15 +0530

    Added transparent button style

--- utils/button_styles.py
+++ utils/button_styles.py
@@ -1,19 +1,19 @@
 from arcade.gui import UIFlatButton
-from utils.colors import BROWN, BEIGE
+from utils.colors import BROWN, BEIGE, TRANSPARENT
 
 
 BUTTON_FONT_NAME = "Pixelzone"
-BUTTON_FONT_SIZE = 30
+SEPIA_BUTTON_FONT_SIZE = 30
 sepia_button_style = {
     "normal": UIFlatButton.UIStyle(
         font_name=BUTTON_FONT_NAME,
-        font_size=BUTTON_FONT_SIZE,
+        font_size=SEPIA_BUTTON_FONT_SIZE,
         font_color=BEIGE,
         bg=BROWN
     ),
     "hover": UIFlatButton.UIStyle(
         font_name=BUTTON_FONT_NAME,
-        font_size=BUTTON_FONT_SIZE,
+        font_size=SEPIA_BUTTON_FONT_SIZE,
         font_color=BEIGE,
         bg=BROWN,
         border=BEIGE,
@@ -21,10 +21,37 @@ sepia_button_style = {
     ),
     "press": UIFlatButton.UIStyle(
         font_name=BUTTON_FONT_NAME,
-        font_size=BUTTON_FONT_SIZE,
+        font_size=SEPIA_BUTTON_FONT_SIZE,
         font_color=BEIGE,
         bg=BROWN,
         border=BEIGE,
         border_width=4
+    )
+}
+
+
+TRANSPARENT_BUTTON_FONT_SIZE = 28
+transparent_button_style = {
+    "normal": UIFlatButton.UIStyle(
+        font_name=BUTTON_FONT_NAME,
+        font_size=TRANSPARENT_BUTTON_FONT_SIZE,
+        font_color=BROWN,
+        bg=TRANSPARENT
+    ),
+    "hover": UIFlatButton.UIStyle(
+        font_name=BUTTON_FONT_NAME,
+        font_size=TRANSPARENT_BUTTON_FONT_SIZE,
+        font_color=BROWN,
+        bg=TRANSPARENT,
+        border=BROWN,
+        border_width=1
+    ),
+    "press": UIFlatButton.UIStyle(
+        font_name=BUTTON_FONT_NAME,
+        font_size=TRANSPARENT_BUTTON_FONT_SIZE,
+        font_color=BROWN,
+        bg=TRANSPARENT,
+        border=BROWN,
+        border_width=2
     )
 }
