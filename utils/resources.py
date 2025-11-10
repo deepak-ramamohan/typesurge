36f7bc79c41b0796a94fc5631d1abc6e2fc223df
Deepak Ramamohan <deepak.r.drm@gmail.com>
2025-11-10 14:09:56 +0530

    Added keypress, error and game over sounds

--- utils/resources.py
+++ utils/resources.py
@@ -13,7 +13,12 @@ USER_PROFILE_SPRITE = arcade.Texture(
     load_image("assets/sprites/account.png", tint_color=BROWN[:3])
 )
 
-# Sounds
+# Music
 MAIN_MENU_MUSIC = arcade.Sound("assets/sounds/hard_boiled.mp3", streaming=True)
 AI_TRAINER_MUSIC = arcade.Sound("assets/sounds/vibing_over_venus.mp3", streaming=True)
 SPACE_SHOOTER_MUSIC = arcade.Sound("assets/sounds/space_jazz.mp3", streaming=True)
+
+# Sounds
+KEYPRESS_SOUND = arcade.Sound("assets/sounds/eklee-KeyPressMac06.wav", streaming=False)
+ERROR_SOUND = arcade.Sound("assets/sounds/error-03-125761.mp3", streaming=False)
+GAME_OVER_SOUND = arcade.Sound("assets/sounds/game-over-417465.mp3", streaming=False)
