fe4d548292b5979848b9ddddfa6d80a6adfa762d
Deepak Ramamohan <deepak.r.drm@gmail.com>
2025-11-12 19:48:24 +0530

    Updated sprites and added laser sound

--- utils/resources.py
+++ utils/resources.py
@@ -1,16 +1,33 @@
 import arcade
-from utils.colors import BROWN
+from utils.colors import BROWN, BEIGE
 from utils.helpers import load_image
 
 
 # Textures
 SEPIA_BACKGROUND = arcade.load_texture("assets/sepia_background.png")
-METEOR_SPRITE_1 = arcade.Texture(load_image("assets/sprites/asteroid.png"))
-METEOR_SPRITE_2 = arcade.Texture(load_image("assets/sprites/meteor.png"))
-SPACESHIP_SPRITE = arcade.Texture(load_image("assets/sprites/spaceship_2.png"))
-BULLET_SPRITE = arcade.Texture(load_image("assets/sprites/bullet.png"))
+METEOR_SPRITE_1 = arcade.load_texture("assets/sprites/meteorBrown_big1.png")
+METEOR_SPRITE_2 = arcade.load_texture("assets/sprites/meteorBrown_big3.png")
+METEOR_SPRITE_3 = arcade.load_texture("assets/sprites/meteorBrown_big4.png")
+SPACESHIP_SPRITE = arcade.Texture(
+    load_image(
+        "assets/sprites/playerShip2_red.png",
+        tint_color=BEIGE[:3]
+    )
+)
+SPACESHIP_DAMAGE_SPRITE_1 = arcade.load_texture("assets/sprites/playerShip2_damage1.png")
+SPACESHIP_DAMAGE_SPRITE_2 = arcade.load_texture("assets/sprites/playerShip2_damage2.png")
+LASER_SPRITE = arcade.Texture(
+    load_image(
+        "assets/sprites/laserRed16.png",
+        tint_color=BEIGE[:3]
+    )
+)
 USER_PROFILE_SPRITE = arcade.Texture(
-    load_image("assets/sprites/account.png", tint_color=BROWN[:3])
+    load_image(
+        "assets/sprites/account.png",
+        invert=True,
+        tint_color=BROWN[:3]
+    )
 )
 
 # Music
@@ -22,3 +39,7 @@ SPACE_SHOOTER_MUSIC = arcade.Sound("assets/sounds/space_jazz.mp3", streaming=Tru
 KEYPRESS_SOUND = arcade.Sound("assets/sounds/eklee-KeyPressMac06.wav", streaming=False)
 ERROR_SOUND = arcade.Sound("assets/sounds/error-03-125761.mp3", streaming=False)
 GAME_OVER_SOUND = arcade.Sound("assets/sounds/game-over-417465.mp3", streaming=False)
+LASER_SOUND = arcade.Sound(":resources:/sounds/laser2.wav", streaming=False)
+
+# Hack: for fixing the initial sound distortion, play something at zero volume
+arcade.play_sound(LASER_SOUND, volume=0)
